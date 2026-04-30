38e13251791292eb7d6042329e3062c629989654
Hamim <41739275+seracoder@users.noreply.github.com>
2026-04-30 17:36:12 +0600

    Bump version

--- src/ibcp/__init__.py
+++ src/ibcp/__init__.py
@@ -2,6 +2,6 @@ from . import models as models
 from .ibcp import REST as REST
 
 # Version is managed in pyproject.toml and automatically synced
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = ["REST", "models"]
